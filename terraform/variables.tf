600cabd9092fd298b1ae7c356f480340342875e5
sherrytp <52416649+sherrytp@users.noreply.github.com>
2024-04-14 23:59:25 -0700

    rebase tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "../.resources/radiant-arcanum-413707-d6bbe40a27e3.json"
+  default     = "./radiant-arcanum-413707-d6bbe40a27e3.json"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
@@ -26,13 +26,13 @@ variable "location" {
 variable "bq_dataset_name" {
   description = "BigQuery dataset name that raw data from GCS will be written to"
   #Update the below to what you want your dataset to be called
-  default     = "stocks_data"
+  default     = "owl_data"
 }
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "stocks_data_bucket"
+  default     = "owl-match-stats"
 }
 
 variable "gcs_storage_class" {
